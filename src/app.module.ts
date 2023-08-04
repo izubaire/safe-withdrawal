c92a67ac7d666b5988cc5bd021b1318034d69b0b
Muhammad Zubair <m.zobaire@gmail.com>
2023-08-04 12:03:31 +0500

    generate module and controller of microservice

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsController } from './cats/cats.controller';
+import { MicroserviceModule } from './microservice/microservice.module';
 
 @Module({
-  imports: [],
+  imports: [MicroserviceModule],
   controllers: [AppController, CatsController],
   providers: [AppService],
 })
